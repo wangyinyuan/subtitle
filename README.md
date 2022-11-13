e9487dd09a12b9012a4d6d11f030eed64926fc23
高 晟捷 <huangguacucumber@outlook.com>
2022-11-13 14:52:41 +0800

    commit
    commit

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# subtitle
+# 字幕
+
+## 灵感
+
+> 一生之中兜兜转转 哪会看清楚 ——李克勤《红日》
+
+去年元旦晚会的时候我们班是电脑上找歌曲mv播放 歌词只有下面一小行 又小又艺术根本看不清楚
+
+然后看到了这个：
+
+![效果图](https://i.postimg.cc/VkSTCX5J/3e8998e1e211b553d5ff0d7b59a455d.jpg)
+
+> 【[U-87]浮誇-陳奕迅（伴奏）-哔哩哔哩】 https://b23.tv/onRZoc9
+
+![效果图](https://i.postimg.cc/FRvn8qpS/98c22c06dce8ff2744da2d4e848e11a.jpg)
+
+> 【居然有人站在讲台上翻唱《浮夸》？！-哔哩哔哩】 https://b23.tv/E5EWfNC
+
+所以就有了灵感 做一个显示字幕的工具
+
+## 技术
+
+### 前端
+
+vue.js + element UI
+
+### 后端
+
+node.js + express + NeteaseCloudMusicApi
+
+
+
+总体上就是请求网易云音乐的api 获取歌词、歌曲url 进行展示
+
+## 实例
+
+[https://subtitle.gaoshengjie.com](https://subtitle.gaoshengjie.com)
---
+++ 部署/app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const path = require("path")
+const { search, lyric, song_url_v1 } = require('NeteaseCloudMusicApi');
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '/public')));
+
+const port = 3000;
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+})
+
+app.post('/api/search', async (req, res) => {
+    try {
+        const result = await search({
+            keywords: req.body.keywords,
+            type: 1,
+            limit: 10
+        })
+        res.json(result)
+    } catch (error) {
+        console.error(error)
+    }
+})
+
+app.post('/api/lyric', async (req, res) => {
+    try {
+        const result = await lyric({
+            id: req.body.id
+        })
+        res.json(result)
+    } catch (error) {
+        console.error(error)
+    }
+})
+
+app.post('/api/songURL', async (req, res) => {
+    try {
+        const result = await song_url_v1({
+            id: req.body.id,
+            level:'exhigh'
+        })
+        console.log(result)
+        res.json(result)
+    } catch (error) {
+        console.error(error)
+    }
+})
+
+app.listen(port, () => {
+    console.log(`The server running at http://localhost:${port}`);
+})
