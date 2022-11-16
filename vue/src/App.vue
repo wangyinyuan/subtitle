4903105b1c07440fd34d673af656ec022509cf28
高 晟捷 <huangguacucumber@outlook.com>
2022-11-16 22:07:43 +0800

    vue源码

---
+++ vue/src/App.vue
@@ -0,0 +1,264 @@
+<script>
+console.log('建立本站初衷是为了庆祝元旦晚会时有歌词放，但我认为无论有没有歌词都是一种美好回忆。试想一下，两年后我们坐在中考考场里，是否还会回忆起那年的欢声笑语？\n\n望中考顺利！\n\n高晟捷，\n2022年11月5日留。')
+async function api(name, parm) {
+    let out = {};
+    // await fetch('http://localhost:3000/api/' + name, {
+    await fetch('/api/' + name, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(parm)
+    }).then(response => response.json()).then(data => {
+        out = data
+    })
+    return out;
+}
+
+export default {
+    data() {
+        return {
+            searchData: [],
+            lyric: '',
+            parsedLyric: [],
+            songURL: '',
+            searchInput: undefined,
+            play: 0,
+            musicTime: 0
+        }
+    },
+    methods: {
+        async search() {
+            if (this.searchInput) {
+                let data = await api('search', { keywords: this.searchInput })
+                if (data.status === 200) {
+                    this.searchData = data.body.result.songs
+                }
+            }
+        },
+        async subtitle(row) {
+            let lyric = await api('lyric', { id: row.id })
+            if (lyric.status === 200) {
+                this.lyric = lyric.body.lrc.lyric;
+                this.parsedLyric = this.parseLyric;
+            }
+            let songURL = await api('songURL', { id: row.id })
+            if (songURL.status === 200) {
+                let url = songURL.body.data[0].url.split(':')
+                url[0] += 's';
+                this.songURL = url.join(':')
+            }
+            let dom = document.getElementById('music');
+            dom.onplay = () => {
+                if (document.documentElement.RequestFullScreen) {
+                    document.documentElement.RequestFullScreen();
+                }
+                if (document.documentElement.mozRequestFullScreen) {
+                    document.documentElement.mozRequestFullScreen();
+                }
+                if (document.documentElement.webkitRequestFullScreen) {
+                    document.documentElement.webkitRequestFullScreen();
+                }
+                if (document.documentElement.msRequestFullscreen) {
+                    document.documentElement.msRequestFullscreen();
+                }
+            }
+            dom.onpause = () => {
+                if (document.exitFullScreen) {
+                    document.exitFullscreen()
+                }
+                //兼容火狐
+                if (document.mozCancelFullScreen) {
+                    document.mozCancelFullScreen()
+                }
+                //兼容谷歌
+                if (document.webkitExitFullscreen) {
+                    document.webkitExitFullscreen()
+                }
+                //兼容IE
+                if (document.msExitFullscreen) {
+                    document.msExitFullscreen()
+                }
+            }
+            document.querySelector('title').innerText = `${row.name} - 字幕`
+            this.play = 1;
+        },
+        setMusicTime() {
+            this.musicTime = document.getElementById('music').currentTime;
+        },
+        isThisLyric(index) {
+            let out = false
+            if (index + 1 <= this.parsedLyric.length - 1) {
+                out = this.parsedLyric[index].time <= this.musicTime && this.parsedLyric[index + 1].time > this.musicTime
+            } else {
+                out = this.parsedLyric[index].time <= this.musicTime
+            }
+            return out
+        },
+        back() {
+            document.getElementById('music').pause();
+            this.play = 0;
+            document.querySelector('title').innerText = '字幕'
+        }
+    },
+    computed: {
+        singers() {
+            let out = [];
+            let singers = this.searchData
+            singers.forEach((song, songIndex) => {
+                song.artists.forEach((singer, singerIndex, arr) => {
+                    if (out[songIndex] === undefined) {
+                        out[songIndex] = ''
+                    }
+                    out[songIndex] += singerIndex == arr.length - 1 ? singer.name : singer.name + "、"
+                })
+            })
+            return out
+        },
+        parseLyric() {
+            function parseLyricLine(line) {
+                const lyricExp = /^\[(\d*):(\d*).(\d*)\](.*)/
+                let result;
+                if ((result = line.match(lyricExp)) !== null) {
+                    return {
+                        time: +result[1] * 60 + +result[2] + +result[3] / 1000,
+                        lyric: result[4].trim()
+                    }
+                }
+            }
+            let lyric = this.lyric
+
+            const lrcObj = []
+            const lrcArr = lyric.split("\n").filter(value => {
+                // 1.通过回车去分割歌词每一行,遍历数组，去除空行空格
+                return value.trim() !== ''
+            }).map(value => {
+                // 2.解析歌词
+                const line = parseLyricLine(value.trim())
+                lrcObj.push(line)
+                return value.trim();
+            })
+            return lrcObj;
+        }
+    }
+}
+</script>
+<template>
+    <div class="search" v-show="!play">
+        <div>
+            <img src="/icon/icon.svg" alt="图标" class="searchIcon">
+        </div>
+        <div class="searchInputGroup">
+            <el-input class="searchInput" size="large" v-model="searchInput" placeholder="请输入歌名" clearable
+                @keyup.enter.native="search" />
+            <el-button size="large" @click="search">搜索</el-button>
+        </div>
+        <div class="searchData" v-if="searchData.length">
+            <el-table class="searchDataTable" :data="searchData" @row-click="subtitle" stripe>
+                <el-table-column prop="name" label="歌名" />
+                <el-table-column label="歌手">
+                    <template #default="scope">
+                        {{ singers[scope.$index] }}
+                    </template>
+                </el-table-column>
+                <el-table-column prop="album.name" label="专辑" />
+            </el-table>
+        </div>
+        <footer>
+            <p class="footerP">高晟捷，版权所有</p>
+            <p class="footerP">
+                <el-link href="https://beian.miit.gov.cn/" target="_blank">鲁ICP备2022004418号-2</el-link>
+            </p>
+            <p class="footerP">
+                <img style="vertical-align:middle" src="https://gaoshengjie.cn/images/ghs.png" width="20" height="20"
+                    alt="公安徽标">
+                <el-link target="_blank"
+                    href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=37028102001219">
+                    <span style="vertical-align:middle">鲁公网安备 37028102001219号</span>
+                </el-link>
+            </p>
+            <p class="footerP">
+                <el-link href="https://www.12377.cn/" target="_blank">中国互联网违法和不良信息举报中心</el-link>
+            </p>
+            <p class="footerP">
+                Made with <span style="color:red;">♥</span> in Qingdao
+            </p>
+        </footer>
+    </div>
+    <div class="play" v-show="play">
+        <div class="lyricList">
+            <span v-for="(item, index) in parsedLyric" v-show="isThisLyric(index)">{{ item.lyric }}</span>
+        </div>
+        <div class="playFooter">
+            <el-button size="large" round @click="back">返回</el-button>
+            <div style="width:2vw"></div>
+            <audio id="music" :src="songURL" controls preload="auto" :ontimeupdate="setMusicTime" />
+        </div>
+    </div>
+</template>
+<style>
+footer {
+    margin-top: 1vw;
+}
+
+.footerP {
+    margin: 0;
+}
+
+.playFooter {
+    position: absolute;
+    bottom: 1vw;
+    display: flex;
+    align-items: center;
+}
+
+.lyricList {
+    font-size: 8vw;
+    padding: 2vw;
+    font-family: fangzhengsong;
+    font-weight: 800;
+}
+
+.lyricList>span {
+    text-align: left;
+}
+
+.play {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+    top: 0;
+    right: 0;
+    background-color: black;
+    color: white;
+    line-height: 8vw;
+}
+
+.searchDataTable {
+    width: 100%;
+    margin-top: 1vw;
+}
+
+.el-table__row {
+    cursor: pointer;
+}
+
+.searchIcon {
+    height: 13vw;
+    min-height: 90px;
+    width: 13vw;
+    min-width: 90px;
+}
+
+.searchInputGroup {
+    display: flex;
+}
+
+.searchInput {
+    width: 40vw;
+}
+</style>
