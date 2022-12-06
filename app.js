a50068a3833f612eced629ab51f43fb2a2dc4ba8
高 晟捷 <huangguacucumber@outlook.com>
2022-12-06 20:35:59 +0800

    增加对注音的支持等
    增加对以下三种语言的四种注音方式支持：
    -粤语(香港语言学会粤语拼音方案)
    -普通话(汉语拼音方案)
    -日语(罗马字)
    -日语(平假名)
    弃用原来臃肿的播放菜单 集成至设置

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const path = require("path")
 const { search, lyric, song_url_v1, album } = require('NeteaseCloudMusicApi');
+
+const { pinyin } = require('pinyin-pro');
+const ToJyutping = require('to-jyutping');
+const Kuroshiro = require("kuroshiro");
+const KuromojiAnalyzer = require("kuroshiro-analyzer-kuromoji");
+const kuroshiro = new Kuroshiro();
+const kuromojiAnalyzer = new KuromojiAnalyzer();
+kuroshiro.init(kuromojiAnalyzer)
+
 const app = express();
 
 app.use(express.json());
@@ -8,7 +17,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 const port = 3000;
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 })
 
@@ -40,7 +49,7 @@ app.post('/api/songURL', async (req, res) => {
     try {
         const result = await song_url_v1({
             id: req.body.id,
-            level:'exhigh'
+            level: 'exhigh'
         })
         res.json(result)
     } catch (error) {
@@ -54,6 +63,83 @@ app.post('/api/album', async (req, res) => {
             id: req.body.id
         })
         res.json(result)
+    } catch (error) {
+        res.json(error)
+    }
+})
+
+app.post('/api/pinyin/jpHiragana', async (req, res) => {
+    try {
+        const originalLyric = req.body.lyric
+        let hiragana = []
+        for (let e of originalLyric) {
+            let lyric = e
+            await kuroshiro.convert(e.lyric, { to: "hiragana", mode: "furigana" })
+                .then(res => { lyric.lyric = res })
+            hiragana.push(lyric)
+        }
+        res.json(hiragana);
+    } catch (error) {
+        res.json(error)
+    }
+})
+
+app.post('/api/pinyin/jpRoma', async (req, res) => {
+    try {
+        const originalLyric = req.body.lyric
+        let roma = []
+        for (let e of originalLyric) {
+            let lyric = e
+            await kuroshiro.convert(e.lyric, { to: "romaji", mode: "furigana" })
+                .then(res => { lyric.lyric = res })
+            roma.push(lyric)
+        }
+        res.json(roma);
+    } catch (error) {
+        res.json(error)
+    }
+})
+
+app.post('/api/pinyin/mandarin', (req, res) => {
+    try {
+        const originalLyric = req.body.lyric
+        let out = []
+        for (let e of originalLyric) {
+            let lyric = e
+            let html = '<ruby>'
+            let py = pinyin(e.lyric, { type: 'array' })
+            for (let i = 0; i < e.lyric.length; i++) {
+                html += `${e.lyric[i]}<rp>(</rp><rt class="mandarinPinyin">${py[i]===e.lyric[i]?'':py[i]}</rt><rp>)</rp>`
+            }
+            html += '</ruby>'
+            lyric.lyric = html;
+            out.push(lyric)
+        }
+        res.json(out);
+    } catch (error) {
+        res.json(error)
+    }
+})
+
+app.post('/api/pinyin/cantonese', (req, res) => {
+    try {
+        const originalLyric = req.body.lyric
+        let out = []
+        for (let e of originalLyric) {
+            let lyric = e
+            let html = '<ruby>'
+            let py = ToJyutping.getJyutpingList(e.lyric)
+            for (let i = 0; i < e.lyric.length; i++) {
+                html += `${py[i][0]}<rp>(</rp><rt>${
+                    py[i][1]===null?'':
+                    parseInt(py[i][1].split(' ')[0].slice(-1))?`${py[i][1].split(' ')[0].slice(0,-1)}<sup>${py[i][1].split(' ')[0].slice(-1)}</sup>`:py[i][1].split(' ')[0]
+                }</rt><rp>)</rp>`
+            }
+            html += '</ruby>'
+            lyric.lyric = html;
+            out.push(lyric)
+        }
+        res.json(out);
     } catch (error) {
         res.json(error)
     }
--- vue/src/App.vue
+++ vue/src/App.vue
@@ -18,18 +18,47 @@ export default {
         return {
             searchData: [],
             searchTableShow: 0,
-            lyric: '',
+            lyric: [],
             parsedLyric: [],
             songURL: '',
             searchInput: undefined,
-            play: 0,
+            play: false,
             musicTime: 0,
             colorBackgroundVisible: true,
-            oldColorBackgroundVisible:true,
-            animationPlayState: 0,
+            oldColorBackgroundVisible: true,
+            animationPlayState: false,
             images: [],
             colorImageSize: [],
-            colorBackgroundImage: []
+            colorBackgroundImage: [],
+            pinyinOptions: [
+                {
+                    value: 'none',
+                    label: '不注音',
+                },
+                {
+                    value: 'cantonese',
+                    label: '粤语(粤拼)'
+                },
+                {
+                    value: 'mandarin',
+                    label: '普通话(汉语拼音)'
+                },
+                {
+                    value: 'jpRoma',
+                    label: '日语(罗马字)'
+                },
+                {
+                    value: 'jpHiragana',
+                    label: '日语(平假名)'
+                },
+            ],
+            pinyinLyric: {
+                mandarin: [],
+                cantonese: [],
+                jpRoma: [],
+                jpHiragana: []
+            },
+            pinyin: 'none',
         }
     },
     methods: {
@@ -43,6 +72,14 @@ export default {
             }
         },
         async subtitle(row) {
+            this.pinyinLyric = {
+                mandarin: [],
+                cantonese: [],
+                jpRoma: [],
+                jpHiragana: []
+            }
+            this.pinyin = 'none'
+            this.animationPlayState = false
             const promiseAlbum = api('album', { id: row.album.id });
             const promiseLyric = api('lyric', { id: row.id })
             const promiseSongURL = api('songURL', { id: row.id })
@@ -106,13 +143,13 @@ export default {
                                 })
                             }
                         }
-                        that.colorBackgroundVisible=that.oldColorBackgroundVisible?true:false;
+                        that.colorBackgroundVisible = that.oldColorBackgroundVisible ? true : false;
                     }
                 }
 
                 const background = async (url) => {
-                    this.oldColorBackgroundVisible=Boolean(`${this.colorBackgroundVisible}`)
-                    this.colorBackgroundVisible=false
+                    this.oldColorBackgroundVisible = Boolean(`${this.colorBackgroundVisible}`)
+                    this.colorBackgroundVisible = false
                     const img = new Image();
                     img.src = url;
                     img.setAttribute("crossOrigin", 'Anonymous')
@@ -123,7 +160,7 @@ export default {
                         const ctx = canvas.getContext('2d');
                         ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
                         const base64 = canvas.toDataURL('image/jpeg');
-                        let splitImage =  new SplitImage({
+                        let splitImage = new SplitImage({
                             row: 2,
                             col: 2,
                             base64
@@ -134,7 +171,7 @@ export default {
             }
             let dom = document.getElementById('music');
             dom.onplay = () => {
-                this.animationPlayState = 1
+                this.animationPlayState = true
                 // 全屏
                 if (document.documentElement.RequestFullScreen) {
                     document.documentElement.RequestFullScreen();
@@ -150,7 +187,7 @@ export default {
                 }
             }
             dom.onpause = () => {
-                this.animationPlayState = 0
+                this.animationPlayState = false
                 // 取消全屏
                 if (document.exitFullScreen) {
                     document.exitFullscreen()
@@ -169,7 +206,7 @@ export default {
                 }
             }
             document.querySelector('title').innerText = `${row.name} - 字幕`
-            this.play = 1;
+            this.play = true;
         },
         setMusicTime() {
             this.musicTime = document.getElementById('music').currentTime;
@@ -178,21 +215,32 @@ export default {
             const lyricTime = this.parsedLyric[index].time;
             const musicTime = this.musicTime
             // 求出下一句时间 解决群青问题
-            let i=1;
-            let nextTime=0;
-            while(nextTime<=lyricTime && index!==this.parsedLyric.length-1){
-                nextTime=this.parsedLyric[index + i].time
+            let i = 1;
+            let nextTime = 0;
+            while (nextTime <= lyricTime && index !== this.parsedLyric.length - 1) {
+                nextTime = this.parsedLyric[index + i].time
                 i++
             }
-            if(musicTime >= lyricTime && (index===this.parsedLyric.length-1 || musicTime < nextTime)){
+            if (musicTime >= lyricTime && (index === this.parsedLyric.length - 1 || musicTime < nextTime)) {
                 return 1;
             }
             return 0
         },
         back() {
             document.getElementById('music').pause();
-            this.play = 0;
+            this.play = false;
             document.querySelector('title').innerText = '字幕'
+        },
+        async pinyinChange(val) {
+            if (JSON.stringify(this.pinyinLyric[val]) == '[]' && val !== 'none') {
+                const data = await api(`pinyin/${val}`, { lyric: this.parsedLyric })
+                this.pinyinLyric[val] = data
+            }
+        },
+        closeSetting(type) {
+            if (type) {
+
+            }
         }
     },
     computed: {
@@ -210,7 +258,7 @@ export default {
             return out
         },
         parseLyric() {
-            function parseLyricLine(line) {
+            const parseLyricLine = line => {
                 const lyricExp = /^\[(\d*):(\d*).(\d*)\](.*)/;
                 let result;
                 if ((result = line.match(lyricExp)) !== null) {
@@ -309,14 +357,14 @@ export default {
             <p class="footerP">
                 来自青岛，用<span style="color:red;">♥</span>制作
             </p>
-            <p class="footerP fontHint">字体使用 <span style="font-family: fangzhengsong">方正清刻本悦宋</span>、<span style="font-family: ms-mincho">明朝体</span></p>
+            <p class="footerP fontHint">字体使用 <span style="font-family: fangzhengsong">方正清刻本悦宋</span>、<span
+                    style="font-family: ms-mincho">明朝体</span></p>
         </footer>
     </div>
     <div class="play" v-show="play">
         <div class="colorBackground background" v-show="colorBackgroundVisible">
             <div class="colorBackgroundContainer">
-                <img class="colorBackgroundImage" v-for="(e, i) in images" :src="e"
-                :style="`
+                <img class="colorBackgroundImage" v-for="(e, i) in images" :src="e" alt="背景" :style="`
                 width:${colorImageSize[1]}px;
                 aspect-ratio:1/1;
                 left:${colorBackgroundImage[i].left}px;
@@ -328,19 +376,55 @@ export default {
         <div class="blackBackground background" v-show="!colorBackgroundVisible"></div>
 
         <div class="lyricList">
-            <span v-for="(item, index) in parsedLyric" v-show="isThisLyric(index)">{{
-                    item.lyric
-            }}</span>
+            <span v-for="(item, index) in pinyin === 'none' ? parsedLyric : pinyinLyric[pinyin]"
+                v-show="isThisLyric(index)" v-html="item.lyric"></span>
         </div>
         <div class="playFooter">
             <el-button size="large" round @click="back">返回</el-button>
             <audio id="music" :src="songURL" controls preload="auto" :ontimeupdate="setMusicTime" />
-            <el-switch class="colorSwitch" v-model="colorBackgroundVisible" size="large" active-text="彩色背景"
-                inactive-text="黑色背景" />
+            <el-dropdown placement="top" trigger="click" class="settings">
+                <span class="el-dropdown-link">
+                    <svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" style="color:rgb(84 84 84);width:30px;height:30px">
+                        <path fill="currentColor"
+                            d="M600.704 64a32 32 0 0 1 30.464 22.208l35.2 109.376c14.784 7.232 28.928 15.36 42.432 24.512l112.384-24.192a32 32 0 0 1 34.432 15.36L944.32 364.8a32 32 0 0 1-4.032 37.504l-77.12 85.12a357.12 357.12 0 0 1 0 49.024l77.12 85.248a32 32 0 0 1 4.032 37.504l-88.704 153.6a32 32 0 0 1-34.432 15.296L708.8 803.904c-13.44 9.088-27.648 17.28-42.368 24.512l-35.264 109.376A32 32 0 0 1 600.704 960H423.296a32 32 0 0 1-30.464-22.208L357.696 828.48a351.616 351.616 0 0 1-42.56-24.64l-112.32 24.256a32 32 0 0 1-34.432-15.36L79.68 659.2a32 32 0 0 1 4.032-37.504l77.12-85.248a357.12 357.12 0 0 1 0-48.896l-77.12-85.248A32 32 0 0 1 79.68 364.8l88.704-153.6a32 32 0 0 1 34.432-15.296l112.32 24.256c13.568-9.152 27.776-17.408 42.56-24.64l35.2-109.312A32 32 0 0 1 423.232 64H600.64zm-23.424 64H446.72l-36.352 113.088-24.512 11.968a294.113 294.113 0 0 0-34.816 20.096l-22.656 15.36-116.224-25.088-65.28 113.152 79.68 88.192-1.92 27.136a293.12 293.12 0 0 0 0 40.192l1.92 27.136-79.808 88.192 65.344 113.152 116.224-25.024 22.656 15.296a294.113 294.113 0 0 0 34.816 20.096l24.512 11.968L446.72 896h130.688l36.48-113.152 24.448-11.904a288.282 288.282 0 0 0 34.752-20.096l22.592-15.296 116.288 25.024 65.28-113.152-79.744-88.192 1.92-27.136a293.12 293.12 0 0 0 0-40.256l-1.92-27.136 79.808-88.128-65.344-113.152-116.288 24.96-22.592-15.232a287.616 287.616 0 0 0-34.752-20.096l-24.448-11.904L577.344 128zM512 320a192 192 0 1 1 0 384 192 192 0 0 1 0-384zm0 64a128 128 0 1 0 0 256 128 128 0 0 0 0-256z">
+                        </path>
+                    </svg>
+                </span>
+                <template #dropdown>
+                    <el-dropdown-menu>
+                        <el-dropdown-item>
+                            <el-switch v-model="colorBackgroundVisible" size="large" active-text="彩色背景"
+                                inactive-text="黑色背景" style="width:100%" />
+                        </el-dropdown-item>
+                        <el-dropdown-item>
+                            <el-select v-model="pinyin" class="m-2" placeholder="注音" size="large"
+                                @change="pinyinChange">
+                                <el-option v-for="item in pinyinOptions" :key="item.value" :label="item.label"
+                                    :value="item.value" :disabled="item.disabled" />
+                            </el-select>
+                        </el-dropdown-item>
+                    </el-dropdown-menu>
+                </template>
+            </el-dropdown>
         </div>
     </div>
 </template>
 <style>
+ruby > rt {
+    text-align: center;
+}
+
+.settings {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+}
+
 footer {
     margin-top: 1vw;
 }
@@ -363,17 +447,15 @@ footer {
     gap: 2vw;
 }
 
-.colorSwitch {
-    background-color: white;
+.playFooter>.el-select>.select-trigger>.el-input>.el-input__wrapper {
+    width: 182px;
     border-radius: 15px;
-    padding-right: 15px;
-    padding-left: 15px;
 }
 
 .lyricList {
     font-size: 8vw;
     padding: 2vw;
-    font-family: fangzhengsong,ms-mincho;
+    font-family: fangzhengsong, ms-mincho;
     font-weight: 800;
     color: white;
     z-index: 0;
@@ -467,5 +549,9 @@ footer {
 
 .searchInput {
     width: 40vw;
+}
+
+.mandarinPinyin {
+    font-family: ms-mincho;
 }
 </style>
